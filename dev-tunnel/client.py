ad7836692c7b743681c36d8711398648816889e4
Chuanbao Lu <chuanbao.lu@hc-sc.gc.ca>
2024-10-25 20:50:09 +0000

    dev container

--- dev-tunnel/client.py
+++ dev-tunnel/client.py
@@ -1,3 +1,9 @@
+from argparse import ArgumentParser
+from dotenv import dotenv_values
+import socket
+import sys
+
+
 def receive_requests():
     # Listen for incoming requests from Server application
     while True:
@@ -7,7 +13,27 @@ def receive_requests():
         # Send response back to Server application
         send_response_to_server(response)
 
+
 def forward_request_to_local_server(request_data):
     # Construct request object based on request_data
     response = requests.request(**request_data)
     return response
+
+
+if __name__ == '__main__':
+    config = dotenv_values('.env')
+    parser = ArgumentParser()
+    parser.add_argument("-s", "--server", dest="server_host", default=config['SERVER_HOST'],
+                        help="Dev-tunnel server host name or IP address")
+    parser.add_argument("-p", "--port", dest="server_port", default=config['SERVER_PORT'],
+                        help="Dev-tunnel server port")
+    args = parser.parse_args()
+
+    print(args.server_host + ':' + args.server_port)
+    # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    #     s.connect((HOST, PORT))
+    #     while True:
+    #         message = input("Enter message: ")
+    #         s.sendall(message.encode())
+    #         data = s.recv(1024)
+    #         print(f"Received from server: {data.decode()}")
--- dev-tunnel/server.py
+++ dev-tunnel/server.py
@@ -1,7 +1,13 @@
 from flask import Flask, request
 import requests
+import socket
+import threading
+
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 65432        # The port used by the server
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def handle_request():
@@ -18,8 +24,37 @@ def handle_request():
     # Return response to browser
     return response
 
+
 def forward_request_to_client(request_data):
     # Establish connection to Client application (e.g., using sockets or a messaging system)
     # Send request data to Client application
     # Receive response from Client application
     return response
+
+
+def handle_client(client_socket, client_address):
+    print(f"New connection from {client_address}")
+    while True:
+        try:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            print(f"Received from {client_address}: {data}")
+            client_socket.sendall(data)
+        except Exception as e:
+            print(f"Error handling client {client_address}: {e}")
+            break
+    client_socket.close()
+    print(f"Connection with {client_address} closed")
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print("Server listening on port", PORT)
+
+    while True:
+        conn, addr = s.accept()
+        client_thread = threading.Thread(
+            target=handle_client, args=(conn, addr))
+        client_thread.start()
